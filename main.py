3236c2d1798d8d3103ee580d34e1762c89855b30
yuuk-1 <nghm9696@gmail.com>
2026-03-25 00:19:17 +0800

    remove le

--- main.py
+++ main.py
@@ -35,8 +35,3 @@ print("halo testing one 2 3")
 print("halo testing one 2 3")
 
 
-print("halo testing one 2 3")
-print("halo testing one 2 3")
-
-print("halo testing one 2 3")
-print("halo testing one 2 3")
